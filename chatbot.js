97d40624c20804e77f92a9dfa312e54707e91608
Spencer G. Crews <sgcrews34@live.com>
2017-06-01 10:28:58 -0500

    Added the handlers for the Intents in LUIS and a default message when an intent is not found

--- chatbot.js
+++ chatbot.js
@@ -16,32 +16,53 @@ var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/4ead4131-
 var recognizer = new builder.LuisRecognizer(model);
 var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
 
-/*
-server.post('/api/messages', connector.listen());
-*/
 
-server.post('/api/messages', function (req, res, next) {
+server.post('/api/messages', connector.listen());
+
+
+/*server.post('/api/messages', function (req, res, next) {
 	console.log(req.body);
-});
+	connector.listen();
+});*/
 
 bot.dialog('/', dialog);
 
-dialog.matches('TestIntent',[
+/*dialog.matches('TestIntent',[
 	function(session, args){
 		var intent = args.intent;
         var email = builder.EntityRecognizer.findEntity(args.entities, 'customer-email');
 		session.send("Your email is " + email);
 	}]);
-
+*/
 dialog.matches('Price',[
 	function(session, args){
 		var intent = args.intent;
         var price = builder.EntityRecognizer.findAllEntities(intent.entities, 'price');
         session.dialogData.searchType = 'price';
-		session.send("Your price is " + price);
+		session.send("Your price is " + "74.99 monthly");//+ price);
 	}]);
 
 dialog.matches('WaitingPeriod',[
 	function(session, args){
 		session.send("A newly created contract takes up to 30 days to become active");
+	}]);
+
+dialog.matches('Address',[
+	function(session, args){
+		session.send("Your Address is 4565 Narrow St. , 38120");
+	}]);
+
+dialog.matches('CustomerEmail',[
+	function(session, args){
+		session.send("Your email is spencer.crews@servicemaster.com");
+	}]);
+
+dialog.matches('None',[
+	function(session, args){
+		session.send("I am sorry I did not quite understand.");
+	}]);
+
+dialog.onDefault([
+	function(session, args, next){
+		session.send("I am sorry I did not quite understand.");
 	}]);
